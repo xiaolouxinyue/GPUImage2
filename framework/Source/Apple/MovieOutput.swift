0f97a895c94fa837b63411bfd9bb34cf0202f522
Jaym <ywjiang1124@163.com>
2020-06-30 19:19:52 +0800

    upgrade to swift 5

--- framework/Source/Apple/MovieOutput.swift
+++ framework/Source/Apple/MovieOutput.swift
@@ -20,8 +20,8 @@ public class MovieOutput: ImageConsumer, AudioEncodingTarget {
     private var videoEncodingIsFinished = false
     private var audioEncodingIsFinished = false
     private var startTime:CMTime?
-    private var previousFrameTime = kCMTimeNegativeInfinity
-    private var previousAudioTime = kCMTimeNegativeInfinity
+    private var previousFrameTime = CMTime.negativeInfinity
+    private var previousAudioTime = CMTime.negativeInfinity
     private var encodingLiveVideo:Bool
     var pixelBuffer:CVPixelBuffer? = nil
     var renderFramebuffer:Framebuffer!
@@ -45,7 +45,7 @@ public class MovieOutput: ImageConsumer, AudioEncodingTarget {
         self.size = size
         assetWriter = try AVAssetWriter(url:URL, fileType:fileType)
         // Set this to make sure that a functional movie is produced, even if the recording is cut off mid-stream. Only the last second should be lost in that case.
-        assetWriter.movieFragmentInterval = CMTimeMakeWithSeconds(1.0, 1000)
+        assetWriter.movieFragmentInterval = CMTimeMakeWithSeconds(1.0, preferredTimescale: 1000)
         
         var localSettings:[String:AnyObject]
         if let settings = settings {
@@ -266,7 +266,7 @@ public extension Timestamp {
     
     var asCMTime:CMTime {
         get {
-            return CMTimeMakeWithEpoch(value, timescale, epoch)
+            return CMTimeMakeWithEpoch(value: value, timescale: timescale, epoch: epoch)
         }
     }
 }
